de005d774d13fab27748ae206462e24316cc03cb
Marc Bigler <mbigler@cmpdesigners.com>
2019-10-13 17:07:15 -0600

    updated test files and created new work branch

---
+++ src/tests/CircleTest.cpp
@@ -0,0 +1,10 @@
+#include <catch2/catch.hpp>
+
+
+TEST_CASE("Checking that valid drone commands", "[MessageCenter]")
+{
+    SECTION("Checking validate()")
+    {
+
+    }
+}
---
+++ src/tests/LineTest.cpp
@@ -0,0 +1,9 @@
+#include <catch2/catch.hpp>
+
+TEST_CASE("Checking that valid drone commands", "[MessageCenter]")
+{
+    SECTION("Checking validate()")
+    {
+
+    }
+}
