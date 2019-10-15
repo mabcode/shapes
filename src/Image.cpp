3c95a254e95765158ccdd01126e401f91656c0fd
Marc Bigler <mbigler@cmpdesigners.com>
2019-10-14 18:35:39 -0600

    updated methods in Virtual Image class to be used for composite pattern

--- src/Image.cpp
+++ src/Image.cpp
@@ -1,2 +1,10 @@
 #include "Image.hpp"
 
+void Image::add(Image *img){}
+void Image::remove(Image *img){}
+Image* Image::getImage(int getImg){}
+void Image::moveX(Image *img){}
+void Image::moveY(Image *img){}
+void Image::computeArea(Image *img){}
+void Image::scale(Image *img){}
+void Image::draw (void){}
--- src/Image.hpp
+++ src/Image.hpp
@@ -4,5 +4,14 @@ class Image {
     private:
 
     public:
-
+        virtual void add(Image *img);
+        virtual void remove(Image *img);
+        virtual Image* getImage(int getImg);
+        virtual void moveX(Image *img);
+        virtual void moveY(Image *img);
+        virtual void computeArea(Image *img);
+        virtual void scale(Image *img);
+        virtual void draw (void);
+        
+        
 };
